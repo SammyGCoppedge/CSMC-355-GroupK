a2d16d95f80df74a22c11ee851fd75537e954bfe
Daniel Lenton <lentondm@vcu.edu>
2018-04-04 15:33:10 -0400

    Create profile page layout

---
+++ app/src/main/java/com/example/grant/groupk/ProfilePage.java
@@ -0,0 +1,13 @@
+package com.example.grant.groupk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProfilePage extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page);
+    }
+}
