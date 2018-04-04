0cc26cbf4f5d09df45a775f7f1f6e908883a8f83
kimmr <33128469+kimmr@users.noreply.github.com>
2018-04-04 19:35:22 -0400

    Added onCalendarClick for calendar

--- app/src/main/java/com/example/grant/groupk/Homepage.java
+++ app/src/main/java/com/example/grant/groupk/Homepage.java
@@ -1,14 +1,17 @@
 package com.example.grant.groupk;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 /**
  * Created by Grant on 4/2/2018.
  */
 
-public class Homepage extends Activity{
+public class Homepage extends AppCompatActivity{ //Activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +21,13 @@ public class Homepage extends Activity{
         String username = getIntent().getStringExtra("Username");
         TextView tv = (TextView)findViewById(R.id.textUsername);
         tv.setText(username);
+    }
+
+    public void onCalendarClick(View v) {
+        if (v.getId() == R.id.CalendarButton)
+        {
+            Intent i = new Intent (Homepage.this, CalendarActivity.class);
+            startActivity(i);
+        }
     }
 }
