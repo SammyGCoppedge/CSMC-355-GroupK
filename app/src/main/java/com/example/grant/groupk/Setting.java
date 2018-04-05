bfb833b1cbcefafc6be7eb16dfd4b32724d088f3
kimmr <33128469+kimmr@users.noreply.github.com>
2018-04-04 23:53:48 -0400

    Added Setting.class

---
+++ app/src/main/java/com/example/grant/groupk/Setting.java
@@ -0,0 +1,17 @@
+package com.example.grant.groupk;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by megry on 4/4/2018.
+ */
+
+public class Setting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+    }
+}
