8d90e977fd808aa2d0109d70cfd520c12e656730
Daniel Lenton <lentondm@vcu.edu>
2018-04-04 23:13:21 -0400

    Add a page to add an event

--- app/src/main/java/com/example/grant/groupk/CalendarActivity.java
+++ app/src/main/java/com/example/grant/groupk/CalendarActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.CalendarView;
 
 /**
@@ -35,5 +36,11 @@ public class CalendarActivity extends AppCompatActivity{
                 startActivity(intent);
             }
         });
+    }
+
+    public void onAddNewEventButtonClick(View v)
+    {
+        Intent i = new Intent(CalendarActivity.this, AddEvent.class);
+        startActivity(i);
     }
 }
