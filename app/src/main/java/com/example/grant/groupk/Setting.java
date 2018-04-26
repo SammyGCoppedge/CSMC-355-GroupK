75b640d4d31bda49d58b5675195732a67a5056dd
kimmr <33128469+kimmr@users.noreply.github.com>
2018-04-26 12:24:42 -0400

    Update Setting

--- app/src/main/java/com/example/grant/groupk/Setting.java
+++ app/src/main/java/com/example/grant/groupk/Setting.java
@@ -1,7 +1,9 @@
 package com.example.grant.groupk;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by megry on 4/4/2018.
@@ -13,5 +15,29 @@ public class Setting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
+    }
+
+    public void onContactClick(View v) {
+        if (v.getId() == R.id.ContactButton)
+        {
+            Intent i = new Intent (Setting.this, Contact.class);
+            startActivity(i);
+        }
+    }
+
+    public void onLogOutClick(View v) {
+        if (v.getId() == R.id.LogOutButton)
+        {
+            Intent i = new Intent (Setting.this, Welcome.class);
+            startActivity(i);
+        }
+    }
+
+    public void onVersionClick(View v) {
+        if (v.getId() == R.id.ContactButton)
+        {
+            Intent i = new Intent (Setting.this, Version.class);
+            startActivity(i);
+        }
     }
 }
