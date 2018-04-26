10c1872ba57b20c693040009dac938968a7b71c3
kimmr <33128469+kimmr@users.noreply.github.com>
2018-04-26 12:25:30 -0400

    Added Version.java

---
+++ app/src/main/java/com/example/grant/groupk/Version.java
@@ -0,0 +1,21 @@
+package com.example.grant.groupk;
+
+/**
+ * Created by megry on 4/26/2018.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
+
+public class Version extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.version);
+    }
+}
